8c0d20dcdbdfdaef767b42f8ade82c7004e216c1
KingSlendy <kingtheslendykilleriwbtg@hotmail.com>
2019-02-12 16:21:26 -0600

    -Changed TP command

--- Assets/scr_sueetWall.cs
+++ Assets/scr_sueetWall.cs
@@ -1,172 +1,171 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text.RegularExpressions;
-using KMBombInfoHelper;
 using UnityEngine;
 
 using Random = UnityEngine.Random;
 using Math = UnityEngine.Mathf;
 
 public class scr_sueetWall : MonoBehaviour {
-	public KMAudio BombAudio;
-	public KMBombInfo BombInfo;
-	public KMBombModule BombModule;
-	public KMSelectable[] ModuleButtons;
-	public KMSelectable ModuleSelect;
-	public Texture2D[] SuitTexs = new Texture2D [0];
+    public KMAudio BombAudio;
+    public KMBombInfo BombInfo;
+    public KMBombModule BombModule;
+    public KMSelectable[] ModuleButtons;
+    public KMSelectable ModuleSelect;
+    public Texture2D[] SuitTexs;
 
-	delegate bool checkCond (int x, int y);
-	bool[] correctButtons = new bool [20];
-	List<int> pressedButtons = new List<int> ();
+    delegate bool checkCond(int x, int y);
+    bool[] correctButtons = new bool[20];
+    List<int> pressedButtons = new List<int>();
 
-	bool moduleSolved;
+    bool moduleSolved;
 
-	static int moduleIdCounter = 1;
-	int moduleId;
+    static int moduleIdCounter = 1;
+    int moduleId;
 
-	void Start () {
-		moduleId = moduleIdCounter++;
+    void Start() {
+        moduleId = moduleIdCounter++;
+        int[] chosenSuits = new int[20];
+        int[] chosenNums = new int[20];
+        int[] chosenColors = new int[20];
 
-		int[] chosenSuits = new int [20];
-		int[] chosenNums = new int [20];
-		int[] chosenColors = new int [20];
+        for (int i = 0; i < ModuleButtons.Length; i++) {
+            chosenSuits[i] = Random.Range(0, SuitTexs.Length);
+            ModuleButtons[i].transform.GetChild(0).GetComponent<Renderer>().material.SetTexture("_MainTex", SuitTexs[chosenSuits[i]]);
+            var suitText = ModuleButtons[i].transform.GetChild(1).GetComponent<TextMesh>();
+            chosenNums[i] = Random.Range(1, 101);
+            suitText.text = chosenNums[i].ToString();
+            chosenColors[i] = Random.Range(0, 2);
+            suitText.color = new[] { Color.black, Color.red }[chosenColors[i]];
+            int j = i;
+            ModuleButtons[i].OnInteract += delegate() {
+                OnButtonPress(j);
 
-		for (int i = 0; i < ModuleButtons.Length; i++) {
-			chosenSuits [i] = Random.Range (0, SuitTexs.Length);
-			ModuleButtons [i].transform.GetChild (0).GetComponent<Renderer> ().material.SetTexture ("_MainTex", SuitTexs [chosenSuits [i]]);
-			var suitText = ModuleButtons [i].transform.GetChild (1).GetComponent<TextMesh> ();
-			chosenNums [i] = Random.Range (1, 101);
-			suitText.text = chosenNums [i].ToString ();
-			chosenColors [i] = Random.Range (0, 2);
-			suitText.color = new [] { Color.black, Color.red } [chosenColors [i]];
+                return false;
+            };
+        }
 
-			int j = i;
+        int[] sideDir = { -6, -5, -4, -1, 1, 4, 5, 6 };
+        int[,] checkSides = {
+            { 0, 2, 5, 7 },
+            { 0, 1, 2, 6 },
+            { 1, 5, 6, 7 },
+            { 1, 3, 4, 6 }
+        };
 
-			ModuleButtons [i].OnInteract += delegate () {
-				OnButtonPress (j);
+        var initTime = (int)BombInfo.GetTime() / 60;
+        checkCond[,] getConds = {
+            { ((x, y) => chosenNums[x] > chosenNums[y]), ((x, y) => chosenNums[x] < initTime), ((x, y) => chosenSuits[x] == chosenSuits[y]), ((x, y) => chosenSuits[x] % 2 == chosenSuits[y] % 2) },
+            { ((x, y) => chosenNums[x] < chosenNums[y]), ((x, y) => chosenNums[x] > initTime), ((x, y) => chosenSuits[x] != chosenSuits[y]), ((x, y) => chosenColors[x] == chosenColors[y]) }
+        };
 
-				return false;
-			};
-		}
+        for (int i = 0; i < ModuleButtons.Length; i++) {
+            var checkSuits = 0;
 
-		int[] sideDir = { -6, -5, -4, -1, 1, 4, 5, 6 };
-		int[,] checkSides = {
-			{ 0, 2, 5, 7 },
-			{ 0, 1, 2, 6 },
-			{ 1, 5, 6, 7 },
-			{ 1, 3, 4, 6 }
-		};
+            for (int j = 0; j < 4; j++) {
+                var checkButton = GetWrapedTable(i, sideDir[checkSides[chosenSuits[i], j]], ModuleButtons.Length, 4);
+                checkSuits += (getConds[chosenColors[i], chosenSuits[i]](checkButton, i)) ? 1 : 0;
+            }
 
-		var initTime = (int)BombInfo.GetTime () / 60;
-		checkCond[,] getConds = {
-			{ ((x, y) => chosenNums [x] > chosenNums [y]), ((x, y) => chosenNums [x] < initTime), ((x, y) => chosenSuits [x] == chosenSuits [y]), ((x, y) => chosenSuits [x] % 2 == chosenSuits [y] % 2) },
-			{ ((x, y) => chosenNums [x] < chosenNums [y]), ((x, y) => chosenNums [x] > initTime), ((x, y) => chosenSuits [x] != chosenSuits [y]), ((x, y) => chosenColors [x] == chosenColors [y]) }
-		};
+            correctButtons[i] = (checkSuits == 4);
 
-		for (int i = 0; i < ModuleButtons.Length; i++) {
-			var checkSuits = 0;
+            if (correctButtons[i]) {
+                Debug.LogFormat(@"[Sueet Wall #{0}] Button on {1} is correct.", moduleId, GetCoord(i));
+            }
+        }
 
-			for (int j = 0; j < 4; j++) {
-				var checkButton = GetWrapedTable (i, sideDir [checkSides [chosenSuits [i], j]], ModuleButtons.Length, 4);
-				checkSuits += (getConds [chosenColors [i], chosenSuits [i]] (checkButton, i)) ? 1 : 0;
-			}
+        if (correctButtons.Count(x => x == true) == 0) {
+            Debug.LogFormat(@"[Sueet Wall #{0}] No buttons are correct, you can press any button.", moduleId);
+        }
+    }
 
-			correctButtons [i] = (checkSuits == 4);
+    int GetWrapedTable(int setButton, int tempDir, int tableSize, int tableRows) {
+        var tempSquare = (Math.Abs(tempDir) == 4) ? -1 * (int)Math.Sign(tempDir) : (Math.Abs(tempDir) % 5) * (int)Math.Sign(tempDir);
 
-			if (correctButtons [i]) {
-				Debug.LogFormat (@"[Sueet Wall #{0}] Button #{1} is correct.", moduleId, i + 1);
-			}
-		}
+        if ((setButton % tableRows) + tempSquare == -1 || (setButton % tableRows) + tempSquare == tableRows) {
+            tempSquare = (tableRows - 1) * ((((setButton % tableRows) + tempSquare) == tableRows) ? -1 : 1);
+        }
 
-		if (correctButtons.Count (x => x == true) == 0) {
-			Debug.LogFormat (@"[Sueet Wall #{0}] No buttons are correct, you can press any button.", moduleId);
-		}
-	}
+        setButton += tempSquare;
 
-	int GetWrapedTable (int setButton, int tempDir, int tableSize, int tableRows) {
-		var tempSquare = (Math.Abs (tempDir) == 4) ? -1 * (int)Math.Sign (tempDir) : (Math.Abs (tempDir) % 5) * (int)Math.Sign (tempDir);
+        if (Math.Abs(tempDir) != 1 && Math.Abs(tempDir) != 0) {
+            tempSquare = tableRows * (int)Math.Sign(tempDir);
+            setButton += tempSquare;
+        }
 
-		if ((setButton % tableRows) + tempSquare == -1 || (setButton % tableRows) + tempSquare == tableRows) {
-			tempSquare = (tableRows - 1) * ((((setButton % tableRows) + tempSquare) == tableRows) ? -1 : 1);
-		}
+        if (setButton < 0) {
+            setButton = tableSize - Math.Abs(setButton);
+        }
 
-		setButton += tempSquare;
+        setButton %= tableSize;
 
-		if (Math.Abs (tempDir) != 1 && Math.Abs (tempDir) != 0) {
-			tempSquare = tableRows * (int)Math.Sign (tempDir);
-			setButton += tempSquare;
-		}
+        return setButton;
+    }
 
-		if (setButton < 0) {
-			setButton = tableSize - Math.Abs (setButton);
-		}
+    string GetCoord(int toCoord) {
+        return string.Format("{0}{1}", (char)((toCoord % 4) + 65), (toCoord / 4) + 1);
+    }
 
-		setButton %= tableSize;
+    void OnButtonPress(int buttonPressed) {
+        BombAudio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        ModuleSelect.AddInteractionPunch();
 
-		return setButton;
-	}
+        if (moduleSolved || pressedButtons.Contains(buttonPressed)) {
+            return;
+        }
 
-	void OnButtonPress (int buttonPressed) {
-		BombAudio.PlayGameSoundAtTransform (KMSoundOverride.SoundEffect.ButtonPress, transform);
-		ModuleSelect.AddInteractionPunch ();
+        var matColor = ModuleButtons[buttonPressed].transform.GetComponent<Renderer>();
 
-		if (moduleSolved || pressedButtons.Contains (buttonPressed)) {
-			return;
-		}
+        if (correctButtons.Count(x => x == true) == 0) {
+            correctButtons[buttonPressed] = true;
+        }
 
-		var matColor = ModuleButtons [buttonPressed].transform.GetComponent<Renderer> ();
+        if (correctButtons[buttonPressed]) {
+            matColor.material.color = new Color32(181, 255, 181, 255);
+            pressedButtons.Add(buttonPressed);
+            Debug.LogFormat(@"[Sueet Wall #{0}] Button on {1} correctly pressed!", moduleId, GetCoord(buttonPressed));
 
-		if (correctButtons.Count (x => x == true) == 0) {
-			correctButtons [buttonPressed] = true;
-		}
+            if (pressedButtons.Count == correctButtons.Count(x => x == true)) {
+                BombModule.HandlePass();
 
-		if (correctButtons [buttonPressed]) {
-			matColor.material.color = new Color32 (181, 255, 181, 255);
-			pressedButtons.Add (buttonPressed);
-			Debug.LogFormat (@"[Sueet Wall #{0}] Button #{1} correctly pressed!", moduleId, buttonPressed + 1);
+                for (int i = 0; i < ModuleButtons.Length; i++) {
+                    ModuleButtons[i].transform.GetComponent<Renderer>().material.color = (correctButtons[i]) ? new Color32(181, 255, 181, 255) : new Color32(128, 191, 255, 255);
+                }
 
-			if (pressedButtons.Count == correctButtons.Count (x => x == true)) {
-				BombModule.HandlePass ();
+                moduleSolved = true;
+                Debug.LogFormat(@"[Sueet Wall #{0}] Module solved!", moduleId);
+            }
+        } else {
+            matColor.material.color = new Color32(128, 191, 255, 255);
+            Debug.LogFormat(@"[Sueet Wall #{0}] Button on #{1} was incorrect.", moduleId, GetCoord(buttonPressed));
+            BombModule.HandleStrike();
+        }
+    }
 
-				for (int i = 0; i < ModuleButtons.Length; i++) {
-					ModuleButtons [i].transform.GetComponent<Renderer> ().material.color = (correctButtons [i]) ? new Color32 (181, 255, 181, 255) : new Color32 (128, 191, 255, 255);
-				}
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} press A1 B2 C3... (buttons to press in columns [A-D] and rows [1-5])";
+    #pragma warning restore 414
 
-				moduleSolved = true;
-				Debug.LogFormat (@"[Sueet Wall #{0}] Module solved!", moduleId);
-			}
-		} else {
-			matColor.material.color = new Color32 (128, 191, 255, 255);
-			Debug.LogFormat (@"[Sueet Wall #{0}] Button #{1} is incorrect.", moduleId, buttonPressed + 1);
-			BombModule.HandleStrike ();
-		}
-	}
+    KMSelectable[] ProcessTwitchCommand(string command) {
+        command = command.ToLowerInvariant().Trim();
 
-	#pragma warning disable 414
-		private readonly string TwitchHelpMessage = @"!{0} press 1 2 3... (buttons to press [within the range 1-20])";
-	#pragma warning restore 414
+        if (Regex.IsMatch(command, @"^press +[a-d1-5^, |&]+$")) {
+            command = command.Substring(6).Trim();
 
-	KMSelectable[] ProcessTwitchCommand (string command) {
-		command = command.ToLowerInvariant ().Trim ();
+            var presses = command.Split(new[] { ',', ' ', '|', '&' }, StringSplitOptions.RemoveEmptyEntries);
+            var pressList = new List<KMSelectable>();
 
-		if (Regex.IsMatch (command, @"^press +[0-9^, |&]+$")) {
-			command = command.Substring (6).Trim ();
+            for (int i = 0; i < presses.Length; i++) {
+                if (Regex.IsMatch(presses[i], @"^[a-d][1-5]$")) {
+                    var setPress = (presses[i][0] - 'a') + (4 * (presses[i][1] - '1'));
+                    pressList.Add(ModuleButtons[setPress]);
+                }
+            }
 
-			var presses = command.Split (new [] { ',', ' ', '|', '&' }, System.StringSplitOptions.RemoveEmptyEntries);
-			var pressList = new List<KMSelectable> ();
+            return (pressList.Count > 0) ? pressList.ToArray() : null;
+        }
 
-			for (int i = 0; i < presses.Length; i++) {
-				if (Regex.IsMatch (presses [i], @"^[0-9]{1,2}$")) {
-					pressList.Add (ModuleButtons [Math.Clamp (Math.Max (1, int.Parse (presses [i].ToString ())) - 1, 0, ModuleButtons.Length - 1)]);
-				}
-			}
-
-			return pressList.ToArray ();
-		}
-
-		return null;
-	}
+        return null;
+    }
 }
